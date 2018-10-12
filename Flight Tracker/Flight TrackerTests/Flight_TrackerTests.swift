e4b83e00a3c3397982e4ad33bad69f9d6643037c
Andrés Medrano <andres97medrano@gmail.com>
2018-10-11 19:52:44 -0700

    added AlamoFire pods

---
+++ Flight Tracker/Flight TrackerTests/Flight_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Flight_TrackerTests.swift
+//  Flight TrackerTests
+//
+//  Created by Andrés Medrano on 10/10/18.
+//  Copyright © 2018 Andrés Medrano. All rights reserved.
+//
+
+import XCTest
+@testable import Flight_Tracker
+
+class Flight_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
